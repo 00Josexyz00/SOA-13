f84587077933dd3ccf379d583eed492223b862ec
Jose Cortez <jose.cortez@epn.edu.ec>
2020-09-09 13:26:44 -0500

    proyecto clase 13

---
+++ frontend/src/app/demo/fabricante/fabricante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fabricante',
+  templateUrl: './fabricante.component.html',
+  styleUrls: ['./fabricante.component.css']
+})
+export class FabricanteComponent implements OnInit {
+  title='FABRICANTE';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
